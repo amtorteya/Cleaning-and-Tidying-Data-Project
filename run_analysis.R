f8d68837f402eee48a30db240b4271b7e8a8acfa
amtorteya <43615532+amtorteya@users.noreply.github.com>
2019-09-30 13:36:15 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 #Global data
 featureNames<-read.table("features.txt",stringsAsFactors=FALSE)[,2]
-featureNamesClean<-gsub("\\-|\\(|\\)","",featureNames)
+featureNamesClean<-gsub("\\-|\\(|\\)","",featureNames) #Remove special characters
 activityLabels<-read.table("activity_labels.txt",stringsAsFactors=FALSE,col.names=c("label","activity"))
 #Train data
 subjectsTrain<-read.table("train/subject_train.txt",col.names="subject")
 featuresTrain<-read.table("train/X_train.txt",col.names=featureNamesClean)
 activityTrain<-read.table("train/y_train.txt",col.names="label")
-activityTrain$activity<-activityLabels$activity[activityTrain$label]
+activityTrain$activity<-activityLabels$activity[activityTrain$label] #Keep the label, not the number
 trainData<-data.frame(subjectsTrain,activity=activityTrain$activity,featuresTrain)
 #Test data
 subjectsTest<-read.table("test/subject_test.txt",col.names="subject")
@@ -24,12 +24,13 @@ activityTidy<-rep(sort(activityLabels$activity),6)
 featuresTidy<-data.frame(matrix(ncol=length(featureNamesClean),nrow=length(subjectsTidy)))
 colnames(featuresTidy)<-featureNamesClean
 dataTidy<-data.frame(subject=subjectsTidy,activity=activityTidy,featuresTidy)
-for(i in 3:ncol(dataSmall))
+for(i in 3:ncol(dataSmall)) #For each feature (except subject and actvity)
 {
-  for(j in 1:length(unique(dataSmall$subject)))
+  for(j in 1:length(unique(dataSmall$subject))) #For each subjects
   {
     dataAux<-subset(dataSmall,dataSmall$subject==j)
-    dataTidy[c((j*6-5):(j*6)),i]<-tapply(dataAux[,i],dataAux$activity,mean)
+    dataTidy[c((j*6-5):(j*6)),i]<-tapply(dataAux[,i],dataAux$activity,mean) #Get and store average per activity
   }
 }
+#Write tidy dataset
 write.table(dataTidy,"dataTidy.txt",row.name=FALSE)
